7129ba95b81c6cc38a34a0dd65e375df8ec4e00a
Yaroslav Yermilov <yermilov17@gmail.com>
2017-03-10 01:37:54 +0200

    #4. groovy-service: try to encode docker credentials inside .travis.yml

--- templates/groovy-service/lazybones.groovy
+++ templates/groovy-service/lazybones.groovy
@@ -1,9 +1,10 @@
 import uk.co.cacoethes.util.NameType
+import org.ini4j.Wini
 
 
 def error = { String message -> throw new IllegalArgumentException(message) }
 
-def askMandatory = { String propertyName, String defaultValue = null ->
+def askParameter = { boolean isOptional, String propertyName, String defaultValue = null ->
     String message = "Define ${transformText(propertyName, from: NameType.PROPERTY, to: NameType.NATURAL)}"
     if (defaultValue != null) {
         message += " [${defaultValue}]"
@@ -11,11 +12,13 @@ def askMandatory = { String propertyName, String defaultValue = null ->
     message += ': '
 
     String answer = ask(message, defaultValue, propertyName)
-    if (answer == null) {
+    if (answer == null && !isOptional) {
         error "Not-null value is required for property named ${propertyName}"
     }
     return answer
 }
+def askMandatory = askParameter.curry(false)
+def askOptional = askParameter.curry(true)
 
 
 def props = [:]
@@ -33,15 +36,24 @@ def environment = [
 ]
 environment.each { props."$it" = '$' + it }
 
-props.serviceName = askMandatory 'serviceName', projectDir.name
-props.serviceDescription = askMandatory 'serviceDescription'
-if (!props.serviceDescription.endsWith('.')) props.serviceDescription += '.'
+def gitUsername
+def gitEmail
+def gitConfigFile = new File(System.getProperty("user.home"), ".gitconfig")
+if (gitConfigFile.exists()) {
+    gitEmail = new Wini(gitConfigFile).get("user", "email")
+}
 
-props.githubOrganization = askMandatory 'githubOrganization'
+props.serviceName = askMandatory 'serviceName', projectDir.name
+props.serviceDescription = askOptional 'serviceDescription'
+if (props.serviceDescription != null && !props.serviceDescription.endsWith('.')) props.serviceDescription += '.'
+
+props.githubOrganization = askMandatory 'githubOrganization', props.serviceName.substring(0, props.serviceName.indexOf('-'))
 props.githubRepository = askMandatory 'githubRepository', props.serviceName
 
 props.dockerhubOrganization = askMandatory 'dockerhubOrganization', props.githubOrganization
 props.dockerhubRepository = askMandatory 'dockerhubRepository', props.githubRepository
+
+props.dockerEmail = askMandatory 'dockerEmail', gitEmail
 
 props.rootPackage = askMandatory 'rootPackage', "com.github.${transformText(props.githubOrganization, from: NameType.HYPHENATED, to: NameType.PROPERTY)}.${transformText(props.githubRepository, from: NameType.HYPHENATED, to: NameType.PROPERTY)}"
 
@@ -58,11 +70,11 @@ def templates = [
   'src/main/resources/logback.groovy',
   'src/docs/howto-start-docker.adoc'
 ]
-
 templates.each { processTemplates it, props }
 
 
 def rootPackageDir = new File(projectDir, "src/main/groovy/${props.rootPackage.replace('.', '/')}")
+rootPackageDir.deleteDir()
 if (!rootPackageDir.mkdirs()) {
   error "Can't create directory ${rootPackageDir.absolutePath}"
 }
@@ -85,3 +97,8 @@ class Application {
 
 
 [ 'controller', 'service' ].each { new File(rootPackageDir, it).mkdirs() }
+
+
+// def encryptDockerEmailProcess = (["cmd.exe", "/c", "travis encrypt DOCKER_EMAIL=${props.dockerEmail}"]).execute([], projectDir)
+// encryptDockerEmailProcess.waitFor()
+// new File(projectDir, 'travis.txt').text = encryptDockerEmailProcess.text
